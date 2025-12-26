7a2bc2a42e7125d1b388674cfbe70965dd4a0537
Tuhin <tb86879@gmail.com>
2025-12-26 16:36:09 +0530

    new

---
+++ test02.js
@@ -0,0 +1,16 @@
+let name= "Tuhin"
+let age = 21
+let isStudent = true
+let club=null
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof undefined)
+console.log(typeof null)
