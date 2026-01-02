41914426e521366b8c3ccc72df039a0b43d95528
Tuhin <tb86879@gmail.com>
2026-01-02 23:59:20 +0530

    object3

--- 02basic/objects_03.js
+++ 02basic/objects_03.js
@@ -1,4 +1,14 @@
 const course={
-    coursename:'js in hindi'
-    
+    coursename:'js in hindi',
+    price:"999",
+    courceInstructor:'hitesh'
 }
+// Old way to access properties
+// console.log(course.courseInstructor);
+
+//modern way to access properties(dysstructuring)
+const{courceInstructor}=course
+console.log(courceInstructor)
+// Destructuring with Renaming
+const{courceInstructor:instructor}=course
+console.log(instructor)
