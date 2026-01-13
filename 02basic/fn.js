a7bf2e33824bcc89367138e4f335da27a7e31b4a
Tuhin <tb86879@gmail.com>
2026-01-13 22:49:19 +0530

    #

--- 02basic/fn.js
+++ 02basic/fn.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+//learn functions
+
+//function declaration
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+console.log(greet("Alice")); // Output: Hello, Alice!
+
+//function expression
+const farewell = function(name) {
+  return "Goodbye, " + name + "!";
+};
+console.log(farewell("Bob")); // Output: Goodbye, Bob!
+//arrow function
+const add = (a, b) => a + b;
+console.log(add(5, 3)); // Output: 8
+//immediate invoked function expression (IIFE)
+(function() {
+  console.log("This function runs immediately!");
+})();
+// Output: This function runs immediately!
+//callback function
+function processUserInput(callback) {
+  const name = "Charlie";
+  console.log(callback(name));
+}
+processUserInput(function(name) {
+  return "Hello, " + name + "!";
+}); // Output: Hello, Charlie!
